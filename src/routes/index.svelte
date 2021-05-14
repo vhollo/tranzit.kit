a1cb72f752e496f74f4395e38399083053834dea
vhollo <vhollo@users.noreply.github.com>
2021-05-14 23:25:41 +0200

    10PM

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -60,7 +60,7 @@
 
       <h1><span lang="hu">Nyitva tartás</span><span lang="en">Opening hours</span></h1>
 
-      <p><span lang="hu">Hétfőtől szombatig: 9.00–21.30</span><span lang="en">Monday to Saturday: 9.00AM to 9.30PM</span></p>
+      <p><span lang="hu">Hétfőtől szombatig: 9.00–22.00</span><span lang="en">Monday to Saturday: 9.00AM to 10.00PM</span></p>
 
       <h5><span lang="hu">Cím:</span><span lang="en">Address:</span> <a rel="noopener" href="https://g.page/tranzitcafe?share">1114
           Budapest, Tranzit Art Café</a></h5>
