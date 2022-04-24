e27454d002b882b8e7361e11cfacc4b3e6e06fb6
vhollo <vhollo@protonmail.com>
2022-04-24 10:04:03 +0200

    no étlap

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -111,7 +111,7 @@
 
         <label class="checked1" for="checked1"><span>BISTRO</span></label>
         
-        <label class="checked2" for="checked2"><span lang="hu">ÉTLAP</span><span lang="en">MENU</span></label>
+        <!--<label class="checked2" for="checked2"><span lang="hu">ÉTLAP</span><span lang="en">MENU</span></label>
         <details>
           <summary><label for="checked2"><span lang="hu">ÉTLAP</span><span lang="en">MENU</span></label></summary>
           <a href="#special">Tranzit Special</a>
@@ -126,7 +126,7 @@
           <a href="#salad"><span lang="hu">Saláták</span><span lang="en">Salads</span></a>
           <a href="#quesadilla"><span lang="hu">Quesadillák</span><span lang="en">Quesadillas</span></a>
           <a href="#alc"><span lang="hu">Alkoholos italok</span><span lang="en">Alcoholic drinks</span></a>
-        </details>
+        </details>-->
 
         <label class="checked3" for="checked3"><span>ART CAFÉ</span></label>
         <label for="lang"><span class="hu">HU</span> / <span class="en">EN</span></label>
@@ -297,7 +297,7 @@
         </radialGradient>
         <path fill="url(#ac)" d="M26.03 10l-.17.15L26 10h.03z" />
       </svg></span><span>BISTRO</span></label>
-  <label class="checked2" for="checked2"><span class="menuicon"><svg xmlns="https://www.w3.org/2000/svg" width="32"
+  <!--<label class="checked2" for="checked2"><span class="menuicon"><svg xmlns="https://www.w3.org/2000/svg" width="32"
         height="32" viewBox="0 0 32 32">
         <radialGradient id="ba" cx="-188" cy="72" r="12.165" gradienttransmenu="matrix(1 0 0 -1 204 88)"
           gradientUnits="userSpaceOnUse">
@@ -336,7 +336,7 @@
           <stop offset="1" stop-color="#1c1b23" />
         </radialGradient>
         <path fill="url(#bg)" d="M22 23s0 1-1 1-1-1-5-1h-2.5c-.28 0-.5-.22-.5-.5s.22-.5.5-.5H21s1 0 1 1z" />
-      </svg></span><span lang="hu">ÉTLAP</span><span lang="en">MENU</span></label>
+      </svg></span><span lang="hu">ÉTLAP</span><span lang="en">MENU</span></label>-->
   <label class="checked3" for="checked3"><span class="menuicon"><svg xmlns="https://www.w3.org/2000/svg" width="32"
         height="32" viewBox="0 0 32 32">
         <radialGradient id="ca" cx="16.5" cy="16.109" r="10.697" gradientUnits="userSpaceOnUse">
